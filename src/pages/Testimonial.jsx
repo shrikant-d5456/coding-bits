ef8dcbb25a6355e1a06fe8a9c4ee7fa3b7ab96d6
shrikant-d5456 <dalvishrikant5456@gmail.com>
2025-01-02 20:41:53 +0530

    updated

--- src/pages/Testimonial.jsx
+++ src/pages/Testimonial.jsx
@@ -1,8 +1,11 @@
-import React, { useEffect, useState } from 'react';
-import { BsSquare, BsSquareFill, BsStarFill } from 'react-icons/bs';
+import React from 'react';
+import { BsStarFill } from 'react-icons/bs';
 import EmptyImg from '../assets/Empty-Image.png';
-// import { Bounce, Fade, Flip, Hinge, JackInTheBox, Roll, Rotate, Slide, Zoom } from 'react-awesome-reveal';
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import '../style/slider.css';
 
+import Slider from "react-slick";
 
 const Testimonial = () => {
   const arr = [
@@ -11,100 +14,100 @@ const Testimonial = () => {
       role: "Web Designer | Software hub PVT. LTD.",
       title: "Amazing Design Inspiration",
       feedback: "The service was amazing. I never had to wait that long for my food. The staff was friendly and attentive.",
-      image: "https://readymadeui.com/profile_2.webp"
+      image: "https://readymadeui.com/profile_2.webp",
+      rating: 4.5,
     },
     {
-      name: "Jane Doe",
-      role: "Frontend Developer | Creative Solutions LLC",
-      title: "Outstanding Service",
-      feedback: "Absolutely loved the experience. The customer service was excellent and the delivery was faster than expected.",
-      image: "https://readymadeui.com/profile_2.webp"
+      name: "Mark Adair",
+      role: "Web Designer | Software hub PVT. LTD.",
+      title: "Amazing Design Inspiration",
+      feedback: "The service was amazing. I never had to wait that long for my food. The staff was friendly and attentive.",
+      image: "https://readymadeui.com/profile_2.webp",
+      rating: 4.5,
     },
     {
-      name: "John Smith",
-      role: "Product Manager | Innovatech Co.",
-      title: "Highly Recommend",
-      feedback: "Their attention to detail is unmatched. I felt valued as a customer and will definitely return.",
-      image: "https://readymadeui.com/profile_2.webp"
+      name: "Mark Adair",
+      role: "Web Designer | Software hub PVT. LTD.",
+      title: "Amazing Design Inspiration",
+      feedback: "The service was amazing. I never had to wait that long for my food. The staff was friendly and attentive.",
+      image: "https://readymadeui.com/profile_2.webp",
+      rating: 4.5,
     },
     {
-      name: "Alice Brown",
-      role: "UX Designer | FlowState Inc.",
-      title: "Exceptional Quality",
-      feedback: "The quality of service was excellent. Staff were very attentive and the food was delicious.",
-      image: "https://readymadeui.com/profile_2.webp"
+      name: "Mark Adair",
+      role: "Web Designer | Software hub PVT. LTD.",
+      title: "Amazing Design Inspiration",
+      feedback: "The service was amazing. I never had to wait that long for my food. The staff was friendly and attentive.",
+      image: "https://readymadeui.com/profile_2.webp",
+      rating: 4.5,
     },
     {
-      name: "Michael Johnson",
-      role: "Backend Developer | CodeCraft Ltd.",
-      title: "Prompt and Professional",
-      feedback: "Service was prompt and professional. The staff went above and beyond to ensure satisfaction.",
-      image: "https://readymadeui.com/profile_2.webp"
-    },
+      name: "Mark Adair",
+      role: "Web Designer | Software hub PVT. LTD.",
+      title: "Amazing Design Inspiration",
+      feedback: "The service was amazing. I never had to wait that long for my food. The staff was friendly and attentive.",
+      image: "https://readymadeui.com/profile_2.webp",
+      rating: 4.5,
+    }
+
   ];
 
-  const [currentIndex, setCurrentIndex] = useState(0);
 
-  const clickIndex = (index) => {
-    setCurrentIndex(index);
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [
+      { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 2 } },
+      { breakpoint: 600, settings: { slidesToShow: 1, slidesToScroll: 1 } },
+    ],
   };
 
-
-
   return (
-    <div className='w-full  dark:bg-slate-900 dark:text-white '>
-      <div className=' sm:w-10/12 w-full h-screen flex flex-col justify-center items-center gap-4 m-auto transition-all duration-200'>
+    <div className="w-full h-screen flex justify-center items-center m-auto dark:bg-slate-900 dark:text-white">
+      <div className="sm:w-10/12 w-full flex flex-col justify-center items-center gap-4 m-auto">
         
-        <p className=' text-4xl font-bold text-center'>Our Experts</p> 
-        
-        <hr className=' border-[1px] w-1/2' />
-        <p className=' text-center'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum illo modi minus earum voluptas unde, totam iusto cupiditate nihil delectus tenetur nam recusandae et fugiat nobis, amet velit rem. Nesciunt dolores fuga ducimus necessitatibus modi debitis nihil velit optio reprehenderit.</p>
-        
-        <div className=" w-[400px] sm:flex justify-center items-center gap-8 p-8 ">
+        <p className="text-4xl font-bold text-center">Our Experts</p>
+        <hr className="border-[1px] w-1/2" />
+        <p className="text-center">
+          
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus suscipit vero officia, numquam sit accusamus repellat cupiditate odit illo saepe sapiente error laborum provident vel dolorem temporibus fugiat magni sed?
+        </p>
+        <Slider {...settings} className="w-full  m-auto">
+          {arr.map((ele, ind) => (
+            <div key={ind} className="flex flex-col justify-center items-center p-6   ">
 
-          <div className="hover:scale-105  overflow-hidden flex flex-col justify-center items-center mb-8 shadow-2xl dark:bg-slate-950 dark:text-white shadow-gray-300 dark:shadow-gray-800 rounded-2xl transition-all cursor-pointer">
+              <div className='w-full flex flex-col justify-center items-center m-auto dark:bg-slate-950 shadow-md shadow-slate-300 dark:shadow-slate-800 rounded-2xl transition-transform hover:scale-105 hover:shadow-xl'>
 
-            <div className=" relative overflow-hidden top-6 gap-2">
-              <img src={EmptyImg || arr[currentIndex].image} alt="img"
-                className="rounded-full w-32 h-32 object-cover bg-gray-100 "
-              />
-              <p className=" absolute bottom-1 text-center px-4 bg-black text-white rounded-2xl left-10  text-sm flex justify-center items-center py-1 gap-1">
-                {arr[currentIndex].rating}
-                <BsStarFill className=" text-yellow-400" />
-              </p>
+                <div className=" flex justify-center items-center mt-4 m-auto">
+                  <img
+                    src={ EmptyImg || ele.image }
+                    alt={`${ele.name}'s profile picture`}
+                    className="rounded-full w-32 h-32 object-cover bg-gray-100"
+                  />
+                </div>
+
+                <div className="flex flex-col px-10 pt-8 text-center gap-2">
+                  <p className="font-bold text-base">{ele.name}</p>
+                  <p className="text-sm text-gray-400">{ele.role}</p>
+                  <p className="font-bold">{ele.title}</p>
+                  <p className="rounded-2xl text-sm px-2 py-1 my-2">{ele.feedback}</p>
+                </div>
+
+                <div className="w-full bg-sky-400 p-2  rounded-b-2xl">
+                  <p className="text-center font-medium text-white">Book a session</p>
+                </div>
+
+              </div>
+
             </div>
-
-            <div className="flex flex-col px-10 pt-8 text-center text-sm gap-y-2">
-              <p className=" font-bold text-base">{arr[currentIndex].name}</p>
-              <p className=" text-sm tracking-wider text-gray-400">{arr[currentIndex].role}</p>
-              <p className=" text-sm font-bold">{arr[currentIndex].title}</p>
-              <p className=" rounded-2xl text-sm px-2 py-1 my-2">{arr[currentIndex].feedback}</p>
-            </div>
-
-            <div className=" w-full bg-sky-400 p-2">
-              <p className=" text-center font-medium text-white">Book a session</p>
-            </div>
-
-          </div>
-
-        </div>
-
-        <div className='flex gap-2 mt-4'>
-          {
-            arr.map((ele, ind) => (
-              <button
-                key={ind} onClick={() => clickIndex(ind)}
-                className=' transition-all duration-2000'
-              >
-                {currentIndex === ind ? <BsSquareFill /> : <BsSquare />}
-              </button>
-            ))
-          }
-
-        </div>
+          ))}
+        </Slider>
       </div>
     </div>
   );
-}
+};
 
 export default Testimonial;
